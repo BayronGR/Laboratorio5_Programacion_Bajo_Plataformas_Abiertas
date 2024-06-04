003a68c6f449aa455e6827d31428b22d32603656
Bayron Guerrero Ruiz <bayrongue78@gmail.com>
2024-06-03 22:30:56 -0600

    Se inserta nodo en posición específica, eliminación de nodo

--- double_list.c
+++ double_list.c
@@ -37,3 +37,52 @@ void insert_at_end(DoubleList* list, int data) {
     }
     list->final = nuevo_nodo;
 }
+
+// Se inserta en una posición específica
+void insert_into_a_position(DoubleList* list, int data, int position) {
+    if (position == 0) {
+        insert_at_start(list, data);
+        return;
+    }
+
+    Nodo* nuevo_nodo = (Nodo*)malloc(sizeof(Nodo));
+    nuevo_nodo->data = data;
+
+    Nodo* actual = list->inicio;
+    for (int i = 0; i < position - 1 && actual != NULL; i++) {
+        actual = actual->siguiente;
+    }
+
+    if (actual == NULL || actual->siguiente == NULL) {
+        insert_at_end(list, data);
+    } else {
+        nuevo_nodo->siguiente = actual->siguiente;
+        nuevo_nodo->anterior = actual;
+        actual->siguiente->anterior = nuevo_nodo;
+        actual->siguiente = nuevo_nodo;
+    }
+}
+
+// Se elimina un nodo
+void delete_by_data(DoubleList* list, int data) {
+    Nodo* actual = list->inicio;
+    while (actual != NULL && actual->data != data) {
+        actual = actual->siguiente;
+    }
+    if (actual == NULL) return;
+
+    if (actual->anterior != NULL) {
+        actual->anterior->siguiente = actual->siguiente;
+    } else {
+        list->inicio = actual->siguiente;
+    }
+
+    if (actual->siguiente != NULL) {
+        actual->siguiente->anterior = actual->anterior;
+    } else {
+        list->final = actual->anterior;
+    }
+
+    free(actual);
+}
+
